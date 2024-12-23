41dd775a123841c03766c9d33535e13f50faa80b
Dimitry-P <dimitry.piatnitsky@yahoo.com>
2024-12-23 16:29:44 +0300

    Add project files.

---
+++ Task_9/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_9
+{
+    internal class Program
+    {
+        public static string number;
+        public static int result;
+        public static int sum = 0;
+        public static int amount = 0;
+
+        //•	Задача 9: Напишите программу, которая находит количество делителей числа.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите любое число больше 0");
+            number = Console.ReadLine();
+            result = int.Parse(number);
+            for (int i = 1; i < result + 1; i++)
+            {
+                if(result % i == 0)
+                {
+                    amount++;
+                }
+            }
+            Console.WriteLine("Кол-во делителей у числа: " + result+" "+ amount);
+        }
+    }
+}
